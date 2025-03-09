bdd43b4554482cbe9aab7c7703905ed2b573cdb4
Raton-dot <152609151+Raton-dot@users.noreply.github.com>
2025-03-09 12:12:00 +0200

    Add files via upload

---
+++ src/utils/generateID.js
@@ -0,0 +1,10 @@
+/**
+ * @license Apache-2.0
+ */
+
+/**
+ * @returns {string} A unique identifier string.
+ */
+export default function generateID() {
+  return Math.random().toString(36).slice(2) + Date.now().toString(36);
+}
